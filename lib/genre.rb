f84998b1b3bd499f536023c790f88ba807e4882a
Serhiy Bardysh <sbardysh@tutanota.com>
2020-07-11 20:05:22 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -19,13 +19,11 @@ class Genre
   end
 
   def songs
-    Song.all.select{|song|song.genre == self} 
+    Song.all.select{|song|song.genre == self}
   end
 
   def artists
-    Song.all.map do |song|
-      song.artist
-    end
+    Song.all.map{|song| song.artist} 
   end
 
 end
